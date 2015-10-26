7fe157aecb9284f46af1388ce5c3bd369b160061
Thomas Asadurian <Tasadurian@gmail.com>
2015-10-25 20:38:30 -0700

    get data function added

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 var request = require('request');
 
 modual.exports = {
+  pokeUrl: 'http://pokeapi.co',
+
+  /**
+   * Get data from pokeapi.
+   *
+   * @param  {String} url
+   * @return {String} JSON data
+   */
+  getData: function(url) {
+    request(url,
+      function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          return body;
+        }
+      });
+  },
 
 };
