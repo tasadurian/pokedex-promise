e1935ae66b1544ef59a1cda731c5c01ed19c697a
Thomas Asadurian <Tasadurian@gmail.com>
2016-01-25 21:11:06 -0800

    got the promise to return data :D

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ var rp = require('request-promise');
 
 var Pokedex = (function() {
   function Pokedex() {}
-
   Pokedex.prototype.getJSON = function() {
     var options = {
-      uri: 'http://pokeapi.co/api/v1/pokedex/1/',
-      json: true
+      url: 'http://pokeapi.co/api/v1/pokedex/1/',
+      json: true,
     };
-
-    return rp(options)
-      .then(function(response) {
-        console.log('getting data');
-        return response;
+    return rp.get(options)
+      .catch(function(error) {
+        return error;
       })
-      .catch(function(err) {
-        console.log('GET failed');
+      .then(function(response) {
+        return response;
       });
   };
+  return Pokedex;
 })();
+
+module.exports = Pokedex;
 
 
 
