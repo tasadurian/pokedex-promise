12bd67df7bf7ba4d91d3042a22a865d6f528b9ec
Thomas Asadurian <Tasadurian@gmail.com>
2016-01-26 20:15:34 -0800

    added getPokemonList

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 var rp = require('request-promise');
 
+var pokeUrl = 'http://pokeapi.co';
+
+getJSON = function(url) {
+  var options = {
+    url: url,
+    json: true,
+  };
+  return rp.get(options)
+    .catch(function(error) {
+      return error;
+    })
+    .then(function(response) {
+      return response;
+    });
+};
+
 var Pokedex = (function() {
   function Pokedex() {}
-  Pokedex.prototype.getJSON = function() {
-    var options = {
-      url: 'http://pokeapi.co/api/v1/pokedex/1/',
-      json: true,
-    };
-    return rp.get(options)
-      .catch(function(error) {
-        return error;
-      })
-      .then(function(response) {
-        return response;
-      });
+
+  Pokedex.prototype.getPokemonList = function() {
+    return getJSON(pokeUrl + '/api/v1/pokedex/1/');
   };
+
   return Pokedex;
 })();
-
-module.exports = Pokedex;
-
-
 
 module.exports = Pokedex;
