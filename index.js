93831b09c61f796348cedc07d92d920c66cfc6b9
Thomas Asadurian <Tasadurian@gmail.com>
2016-02-02 21:19:06 -0800

    added type, move, ability, egg, description, and sprite

--- index.js
+++ index.js
@@ -27,6 +27,30 @@ var Pokedex = (function() {
     return getJSON(pokeUrl + '/api/v1/pokemon/' + id + '/');
   };
 
+  Pokedex.prototype.getTypeById = function(id) {
+    return getJSON(pokeUrl + '/api/v1/type/' + id + '/');
+  };
+
+  Pokedex.prototype.getMoveById = function(id) {
+    return getJSON(pokeUrl + '/api/v1/move/' + id + '/');
+  };
+
+  Pokedex.prototype.getAbilityById = function(id) {
+    return getJSON(pokeUrl + '/api/v1/ability/' + id + '/');
+  };
+
+  Pokedex.prototype.getEggById = function(id) {
+    return getJSON(pokeUrl + '/api/v1/egg/' + id + '/');
+  };
+
+  Pokedex.prototype.getDescriptionById = function(id) {
+    return getJSON(pokeUrl + '/api/v1/description/' + id + '/');
+  };
+
+  Pokedex.prototype.getSpriteById = function(id) {
+    return getJSON(pokeUrl + '/api/v1/sprite/' + id + '/');
+  };
+
   return Pokedex;
 })();
 
